673fb43d4f851fbfda31f0b7579ad7b0067db87b
Den-Skochok <denis93skochok@gmail.com>
2022-07-31 11:16:28 +0300

    Correction according to comments

--- src/FirstHomeWork.java
+++ src/FirstHomeWork.java
@@ -4,43 +4,40 @@ public class FirstHomeWork {
 
     private static int firstDigit;
     private static int secondDigit;
-    public static String mathAction;
+    private static String mathAction;
 
-    public static float result;
-    public static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        System.out.println("Please enter first Digit:");
+        System.out.print("Please enter first Digit: ");
         firstDigit = scanner.nextInt();
-//        if(scanner.hasNextInt()){
-//            System.out.println("Please enter action +, -, *, / :");
-//            mathAction = scanner.next();
-//        } else {
-//            System.out.println("You can enter only digits. Try again: ");
-//            firstDigit = scanner.nextInt();
-//        }
 
-        System.out.println("Please enter action +, -, *, / :");
+        System.out.print("Please enter action +, -, *, / : ");
         mathAction = scanner.next();
 
-        System.out.println("Please enter second Digit:");
+        System.out.print("Please enter second Digit: ");
         secondDigit = scanner.nextInt();
 
-        if (mathAction.equals("+")) {
-            System.out.println("Result: " + firstDigit + " " + mathAction + " " + secondDigit + " = " + (firstDigit + secondDigit));
-        } else if (mathAction.equals("-")) {
-            System.out.println("Result: " + firstDigit + " " + mathAction + " " + secondDigit + " = " + (firstDigit - secondDigit));
-        } else if (mathAction.equals("*")) {
-            System.out.println("Result: " + firstDigit + " " + mathAction + " " + secondDigit + " = " + (firstDigit * secondDigit));
-        } else if (mathAction.equals("/")) {
-            if (secondDigit == 0) {
-                System.out.println("Cannot be divided by 0");
-            } else {
-                System.out.println("Result: " + firstDigit + " " + mathAction + " " + secondDigit + " = " + ((float)firstDigit / secondDigit));
-            }
-        } else if(!mathAction.equals("+") || !mathAction.equals("-") || !mathAction.equals("*") || !mathAction.equals("/")){
-            System.out.println("Enter correct math Action from the proposed");
+        switch (mathAction) {
+            case ("+"):
+                System.out.println("Result: " + firstDigit + " " + mathAction + " " + secondDigit + " = " + (firstDigit + secondDigit));
+                break;
+            case "-":
+                System.out.println("Result: " + firstDigit + " " + mathAction + " " + secondDigit + " = " + (firstDigit - secondDigit));
+                break;
+            case "*":
+                System.out.println("Result: " + firstDigit + " " + mathAction + " " + secondDigit + " = " + (firstDigit * secondDigit));
+                break;
+            case "/":
+                if (secondDigit == 0) {
+                    System.out.println("Cannot be divided by 0");
+                } else {
+                    System.out.println("Result: " + firstDigit + " " + mathAction + " " + secondDigit + " = " + ((float) firstDigit / secondDigit));
+                }
+                break;
+            default:
+                System.out.println("Enter correct math Action from the proposed");
         }
     }
 }
